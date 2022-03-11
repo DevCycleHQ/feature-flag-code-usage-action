a046608a6a6697eac553bde76c6a36b33180ade0
Laura Warr <laura.warr@taplytics.com>
2022-03-11 07:53:36 -0800

    Update cli version and example_output image

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ async function run() {
     }
 
     try {
-        await exec('npm', ['install', '-g', '@devcycle/cli@2.1.0-alpha.2'])
+        await exec('npm', ['install', '-g', '@devcycle/cli@3.0.0'])
 
         const output = await getExecOutput(
             'dvc',
