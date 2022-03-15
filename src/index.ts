b34d262d30053f53a65ff81d3729411ef16383d5
Laura Warr <laura.warr@taplytics.com>
2022-03-15 09:33:35 -0700

    CLI version 3.0.1

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ async function run() {
     }
 
     try {
-        await exec('npm', ['install', '-g', '@devcycle/cli@3.0.0'])
+        await exec('npm', ['install', '-g', '@devcycle/cli@3.0.1'])
 
         const output = await getExecOutput(
             'dvc',
