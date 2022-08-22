ad530ba0f16f0c774eb73530cce6ceff005ccf5c
Laura Warr <laura.warr@taplytics.com>
2022-08-22 14:41:33 -0700

    Bump cli version

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ async function run() {
     }
 
     try {
-        await exec('npm', ['install', '-g', '@devcycle/cli@4.0.0'])
+        await exec('npm', ['install', '-g', '@devcycle/cli@4.2.3'])
 
         const output = await getExecOutput(
             'dvc',
