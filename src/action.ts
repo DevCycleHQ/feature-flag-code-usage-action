f9fd808dba8d4ecce8507a3ce02c0f88b5998b07
Laura Warr <laura.warr@taplytics.com>
2023-05-01 08:09:05 -0700

    Bump cli to 4.2.11

--- src/action.ts
+++ src/action.ts
@@ -26,7 +26,7 @@ export async function run() {
     }
 
     try {
-        await exec('npm', ['install', '-g', '@devcycle/cli@4.2.10'])
+        await exec('npm', ['install', '-g', '@devcycle/cli@4.2.11'])
 
         const output = await getExecOutput(
             'dvc',
