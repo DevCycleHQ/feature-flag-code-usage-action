8635dd373fe713772cdb68e0a1971361d0e280b8
Parth Suthar <parth.suthar@taplytics.com>
2024-05-10 11:08:14 -0400

    fix: code usages not reporting

--- dist/index.js
+++ dist/index.js
@@ -63,7 +63,7 @@ function run() {
             return;
         }
         try {
-            yield (0, exec_1.exec)('npm', ['install', '-g', '@devcycle/cli@5.10.1']);
+            yield (0, exec_1.exec)('npm', ['install', '-g', '@devcycle/cli@5.14.9']);
             const output = yield (0, exec_1.getExecOutput)('dvc', ['usages', '--format', 'json', '--caller', DVC_IDENTIFIER]);
             const variables = JSON.parse(output.stdout);
             yield action.postCodeUsages(variables);
